67bebe56164baba50e64783faa39ded7d0d022b2
CTPEKOTE <31778376+CTPEKOTE@users.noreply.github.com>
2017-09-30 15:16:12 +0300

    Add files via upload

--- receiver.c
+++ receiver.c
@@ -5,64 +5,68 @@
 #include <string.h>
 #include <signal.h>
 int buffer = 0, flip = 0;
-void sighandler (int signo) {
+void sighand (int signo){
 
-	flip = 1;
+        printf("received signal\n");
+
+        return;
+}
+void sighandler (int signo){
 	
 	buffer *= 2;
+
+	printf("received 0\n");
 
 	return;
 }
 void sighandler1 (int signo) {
-	
-	flip = 1;
 
 	buffer *= 2;
 	
 	buffer += 1;
 
+	printf("received 1\n");
 
 	return;
 }
 int main(int argc, char **argv){
 	int pid , k;
 	char i = 0;
-	printf("pid is %d\n",pid = getpid());
+	sigset_t wait;
+	sigemptyset(&wait);
+	sigaddset(&wait , SIGINT);
+	sigaddset(&wait , SIGILL);
+
+	printf("pid is %d\n",k = getpid());
 	scanf("%d",&pid);
 	
 	signal(SIGINT , sighandler);
 	signal(SIGILL , sighandler1);
+	signal(SIGUSR1,sighand);
+
 	
 	while(0 == 0){
-		
-		if( flip == 1 ){
 
-			for (k = 0; k < 8; k ++){
-				usleep(1000);
-				fprintf(stdin," ");
+		for (k = 0; k < 8; k ++){
+			printf("start of receiving\n");
+			sigsuspend(&wait);
+			printf("bit received\n");
+			usleep(9999);
+//			sigsuspend(&wait);
+			kill(pid, SIGUSR2);
 
-			}
-		
-
-			for(k = 0; k < 8; k ++){
-				i *= 2;
-				i += buffer % 2;
-				buffer /= 2;
-				
-			}
-
-			printf("%c",i);
-			i = 0;	
-
-			flip = 0;
 		}
-		
-		usleep(1000);
-		fprintf(stdin,"\n");
-		
+		printf("end of receiving\n");
+
+		for(k = 0; k < 8; k ++){
+			i *= 2;
+			i += buffer % 2;
+			buffer /= 2;
+			
+		}
+		printf("%c",i);
+		i = 0;	
+		buffer = 0;	
 	}
-	
-	
-	
-	return 0;
+		return 0;
 }
--- sender.c
+++ sender.c
@@ -4,12 +4,25 @@
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+void sighand (int signo){
+
+	printf("received signal\n");
+
+	return;
+}
 int main(int argc, char **argv){
-	int pid , i = 0, k;
+	int pid , i = 0, k, sig;
 	char buffer[100];
+	sigset_t wait;
+	
+	sigemptyset(&wait);
+	sigaddset(&wait, SIGINT);
+
 	printf("pid is %d\n",k = getpid());
 	scanf ("%d",&pid);
 
+	signal(SIGUSR2,sighand);
+	
 	while ( 0 == 0 ){
 		scanf("%c",buffer);
 		while( buffer[i]  != '\n'){
@@ -21,19 +34,33 @@ int main(int argc, char **argv){
 	
 		buffer[i + 1] = 0;		
 		
+		printf("end of scanf\n");
 
 		while ( i >= 0) {
 	
 			for(k = 0 ; k < 8; k++){
-			
-				if (*buffer % 2 == 0)
-					kill(pid , SIGINT);
-				else 
-					kill(pid , SIGILL);
-			
-				*buffer /= 2;
 
-				usleep(1000);
+				kill(pid , SIGUSR1);
+			
+				printf("starting desintegration\n");
+
+				if (*buffer % 2 == 0){
+					kill(pid , SIGINT);
+					printf("0 sent\n");
+				}
+				else {
+					kill(pid , SIGILL);
+					printf("1 sent\n");
+				}		
+				
+				buffer[0] /= 2;
+
+				printf("in process\n");
+
+				if (k = sigsuspend(&wait) == -1)
+					exit(1);
+
+				printf("ending desintegration\n");
 	
 			}
 			
@@ -42,8 +69,6 @@ int main(int argc, char **argv){
 				buffer[k] = buffer[k + 1];
 			}
 
-			usleep(1000);
-		
 			i--;
 			
 		}
