1534c479e1033abfc8b361bfd31c530f71401ad7
Malonosova <32372730+Malonosova@users.noreply.github.com>
2017-11-09 06:31:35 +0400

    Add files via upload

--- shm_a.c
+++ shm_a.c
@@ -9,40 +9,30 @@
 #include <string.h>
 int main(int argc, char** argv)
 {
-	int shmid, semid;
-	int* arg;
+    int* arg, shmid;
+	key_t key2 = 123;
+    shmid = shmget(key2, sizeof(int), 0666 | IPC_CREAT);
+    arg = (int *)shmat(shmid, NULL, 0);
 
-	key_t key1 = 124, key2 = 123;
-
-	struct sembuf mybuf;
-
-	if ((semid = semget(key1 , 2, 0666 | IPC_CREAT)) < 0)
-	{
-		printf("Can`t get semid\n");
-		exit(-1);
-	}
-
-	if ((shmid = shmget(key2, sizeof(int), 0666 | IPC_CREAT)) < 0)
-	{
-		printf("Can't create shared memory\n");
-		exit(-1);
-	}
-	if ((arg = (int *)shmat(shmid, NULL, 0)) == (int *)(-1))
-	{
-		printf("Can't attach shared memory\n");
-		exit(-1);
-	}
 	(*arg) = atoi(argv[1]);
+
+	int semid;
+	key_t key1 = 125;
+	struct sembuf mybuf;
+	semid = semget(key1 , 1, 0666 | IPC_CREAT);
+
+    mybuf.sem_op = 1;
+    mybuf.sem_flg = 0;
+    mybuf.sem_num = 0;
+    semop(semid, &mybuf, 1);
+
+    key1 = 124;
+	semid = semget(key1 , 1, 0666 | IPC_CREAT);
 
 	mybuf.sem_op = -1;
 	mybuf.sem_flg = 0;
-	mybuf.sem_num = 1;
-
-	if (semop(semid , &mybuf , 1) < 0)
-	{
-		printf("Can`t wait for condition\n");
-		exit(-1);
-	}
+	mybuf.sem_num = 0;
+	semop(semid , &mybuf , 1);
 	printf ("%d\n", *arg);
 	return 0;
 }
--- shm_b.c
+++ shm_b.c
@@ -9,33 +9,26 @@
 #include <errno.h>
 
 int main() {
-	int shmid, semid;
-	key_t key2 = 123, key1 = 124;
+	int semid;
+	key_t key1 = 125;
 	struct sembuf mybuf;
-	int* arg;
-	if ((semid = semget(key1 , 2, 0666 | IPC_CREAT)) < 0)
-	{
-		printf("Can`t get semid\n");
-		exit(-1);
-	}
-        if ((shmid = shmget(key2, sizeof(int), 0666 | IPC_CREAT)) < 0)
-        {
-                printf("Can't create shared memory\n");
-                exit(-1);
-        }
-        if ((arg = (int *)shmat(shmid, NULL, 0)) == (int *)(-1))
-        {
-                printf("Can't attach shared memory\n");
-                exit(-1);
-        }
-        *arg = (*arg) * (*arg);
+	semid = semget(key1 , 1, 0666 | IPC_CREAT);
+    mybuf.sem_op = -1;
+    mybuf.sem_flg = 0;
+    mybuf.sem_num = 0;
+    semop(semid, &mybuf, 1);
 
+    key1 = 124;
+	semid = semget(key1 , 1, 0666 | IPC_CREAT);
+
+    int* arg, shmid;
+	key_t key2 = 123;
+    shmid = shmget(key2, sizeof(int), 0666 | IPC_CREAT);
+    arg = (int *)shmat(shmid, NULL, 0);
+    *arg = (*arg) * (*arg);
 	mybuf.sem_op = 1;
 	mybuf.sem_flg = 0;
-	mybuf.sem_num = 1;
-	if (semop(semid , &mybuf , 1) < 0) {
-		printf("Can`t wait for condition\n");
-		exit(-1);
-	}
+	mybuf.sem_num = 0;
+	semop(semid , &mybuf , 1);
 	return 0;
 }
